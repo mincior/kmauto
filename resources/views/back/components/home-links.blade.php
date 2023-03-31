bec3b6a0061a9bcd03d8f8370e685c81aab3b8c8
Vicentiu MINCIOR <mincior@yahoo.com>
2023-03-31 10:18:30 +0300

    Modificare pagina de start

--- resources/views/back/components/home-links.blade.php
+++ resources/views/back/components/home-links.blade.php
@@ -1,66 +1,69 @@
 <div class="card mb-3">
-    <div class="card-header">
+    <div class="card-header flex">
         <div class="row">
-            <div class="col">Km auto</div>
 
             <div class="col fs-5 text-end">
                 <img src="{{ asset('img/icons/link.png') }}" />
                 <img src="{{ asset('img/icons/home.png') }}" />
             </div>
         </div>
+        <div >Km Auto. Copyright - Consolight SRL: Vicentiu Mincior, Razvan Duinea, Petrica Ilie @ 2023</div>
     </div>
 
     <div class="card-body">
-        Aceasta aplicatie a fost realizata pentru a tine evidenta kilometrajelor masinilor Consolight pe perioadele de weekend.
-        De asemenea, pentru a pune la plata soferii care folosesc aceste masini in interes personal tot in wekend.
+        Scopul aplicatiei este sa gestioneze deplasarile masinilor consolight pe perioada de weekend si sarbatori legale, pentru a pune 
+        la plata deplasarile in scop personal ale soferilor si mai ales pentru a percepe impozit pentru acest ‘beneficiu’ acordat
+        salariatilor ce detin in custodie o masina apartinand Consolight SRL
         <hr />
-        This application is <a href="https://www.php.net/" target="_blank">PHP 8.1</a> compatible and build using :
+        Aceasta aplicatie este <a href="https://www.php.net/" target="_blank">PHP 8.1</a> compatibila si a fost construita folosind :
         <ul>
-            <li><a href="https://laravel.com/" target="_blank">Km Auto</a></li>
+            <li><a href="https://laravel.com/" target="_blank">Laravel</a></li>
             <li><a href="https://getbootstrap.com/" target="_blank">Bootstrap 5</a></li>
             <li><a href="https://datatables.net/" target="_blank">DataTables</a></li>
         </ul>
         <hr />
-        <b>Features</b> :
+        <b>Optiuni</b> :
         <ul>
-            <li><b>Top button bar</b> to quickly navigate to the main parts of your application</li>
-            <li><b>Offcanvas menu</b> to access less frequently used parts</li>
-            <li><b>Datatables</b>, fully integrated with build in :</li>
+            <li><b>Bara de navigare superioara</b> Pentru a naviga rapid in diversele parti ale aplicatiei</li>
+            <li><b>Meniu lateral</b> pentru a accesa optiunile folosite mai rar</li>
+            <li><b>Utilizatori</b> accesare de pe telefonul mobil printr-ul link plasat pe ecranul principal :</li>
             <ul>
-                <li>Create - Show - Update - Delete (CRUD) with
-                    <a href="http://bootboxjs.com/" target="_blank">Bootbox.js</a> dialogs and
-                    <a href="https://codeseven.github.io/toastr/" target="_blank">Toastr</a> notifications
+                <li>Logare in aplicatie pe baza de adresa de email si parola 
+                    (care vor fi salvate in telefon pentru a nu fi introduse de fiecare data manual)
                 </li>
-                <li>Copy to clipboard</li>
-                <li>Export to PDF and Excel</li>
-                <li>Print function</li>
-                <li>Items per page selector</li>
-                <li>Column visibility selector</li>
-                <li>Search with result highlighting</li>
-                <li>Server-side pagination and filtering</li>
-                <li>Multiple row selection (for mass deletes)</li>
-                <li>Inline boolean field toggable</li>
-                <li>Help</li>
+                <li>Selectare luna si interval de weekend pentru care se fac operatii</li>
+                <li>Daca sunt mai multe operatii, ordinea acestora se va putea modifica grafic 
+                    dupa pozitia lor in tabel</li>
+                <li>Modificare inregistari (km, status, observatii) precum si posibilitaea mutarii in alt interval</li>
+                <li>Stergerea, adaugarea de inregistrari</li>
+                <li>Un raport sumar cu numarul de km parcursi in luna selectata precum si o estimare a sumei de plata
+                    bazata pe pretul combustibilului din luna anterioara (daca luna este deschisa sau din luna selectata daca luna s-a inchis)
+                </li>
+                <li>Cautare in inregistrari cu evidentiere a cuvantului cautat</li>
+                <li>Informatia este organizata pe pagini</li>
+                <li></li>
+                <li></li>
+                <li></li>
             </ul>
         </ul>
         <hr />
-        <b>Special feature</b> :
-        <p>The top menu contains in its center a dropdown selector for the year. This is implemented as a global session
-            variable <b>[APP].[YEAR]</b> and allows you to easely filter datatable datasets (when needed) to reflect the
-            data concerning the selected year. This is extreem helpfull if you manage models that depend on the year,
-            like for instance deliveries, orders, productions, and so on...
+        <b>Optiune speciala</b> :
+        <p>Bara de navigare de sus contine un selector pentru luna curenta si unul pentru intervalele de weekend ale lunii selectate.
+            Acestea permit alegerea 'lunii curente' pentru care se fac prelucrari, introduceri de date, luna fiind editabila daca inca nu s-au transmis
+            rapoartele la financiar sau 'doar in citire' daca da.
         </p>
     </div>
 
     <div class="card-footer">
         <div class="row">
-            <div class="col text-danger">
+            <div class="col text-start md-6">
+                <h5 id="MyClockDate" onload="showDate();"></h5>
+            </div>
+
+            <div class="col text-danger text-end">
                 <h5 id="MyClockTime" onload="showDate();"></h5>
             </div>
 
-            <div class="col text-end">
-                <h5 id="MyClockDate" onload="showDate();"></h5>
-            </div>
         </div>
     </div>
 </div>
