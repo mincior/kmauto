cd23ad48f5f1e691548117b68a1aa69e25b4427f
Vicentiu MINCIOR <mincior@yahoo.com>
2023-03-31 09:28:28 +0300

    aduagare select 1 pe una din inregistrarile migrarii months

--- database/seeders/MonthTableSeeder.php
+++ database/seeders/MonthTableSeeder.php
@@ -28,23 +28,23 @@ class MonthTableSeeder extends Seeder
         $users = Month::factory()->state(
             new Sequence(
                 [
-                    'id' => 1, 'data_raportarii' => '2022-12-01', 'anul_luna' => 'noiembrie 2022',
+                    'id' => 1, 'data_raportarii' => '2022-12-01', 'anul_luna' => 'noiembrie 2022', 'select' => 0,
                 ],
 
                 [
-                    'id' => 2, 'data_raportarii' => '2023-01-01', 'anul_luna' => 'decembrie 2022',
+                    'id' => 2, 'data_raportarii' => '2023-01-01', 'anul_luna' => 'decembrie 2022','select' => 0,
                 ],
 
                 [
-                    'id' => 3, 'data_raportarii' => '2023-02-01', 'anul_luna' => 'ianuarie 2023',
+                    'id' => 3, 'data_raportarii' => '2023-02-01', 'anul_luna' => 'ianuarie 2023','select' => 0,
                 ],
 
                 [
-                    'id' => 4, 'data_raportarii' => '2023-03-01', 'anul_luna' => 'februarie 2023',
+                    'id' => 4, 'data_raportarii' => '2023-03-01', 'anul_luna' => 'februarie 2023','select' => 0,
                 ],
 
                 [
-                    'id' => 5, 'data_raportarii' => '2023-04-01', 'anul_luna' => 'martie 2023',
+                    'id' => 5, 'data_raportarii' => '2023-04-01', 'anul_luna' => 'martie 2023','select' => 1,
                 ],
 
             )
